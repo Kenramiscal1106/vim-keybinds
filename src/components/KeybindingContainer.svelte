b227d6508331b1593eb4dd6910ba3a2fdb25e09a
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-31 08:49:59 +0800

    refactor: :lipstick: add stying and change element

--- src/components/KeybindingContainer.svelte
+++ src/components/KeybindingContainer.svelte
@@ -6,8 +6,8 @@
   export let keybinds: KeybindObj[];
 </script>
 
-<div class="divide-y divide-neutral-600 px-6 my-5">
+<table class="divide-y divide-neutral-600 px-6 my-5 max-w-5xl">
   {#each keybinds as keybind}
     <KeybindingWrapper {keybind} />
   {/each}
-</div>
+</table>
